9492f7e12e24b8a029645349057b4482fc5b87cd
James kamau <jnk13576@gmail.com>
2025-09-07 17:52:28 +0300

    added authentications

--- django-models/LibraryProject/bookshelf/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import BookSuggestion
-
-class BookSuggestionForm(forms.ModelForm):
-    class Meta:
-        model = BookSuggestion
-        fields = ['title', 'author', 'publication_year', 'status']
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from . import views
 from .views import list_books
 from .views import LibraryDetailView
+from django.contrib.auth import views as auth_views
 
 urlpatterns=[
-    path('', list_books, name='list_book'),
-    path('library/<int:pk>/',LibraryDetailView.as_view(), name='library_details')
+    path('', list_books, name='home'),
+    path('library/<int:pk>/',LibraryDetailView.as_view(), name='library_details'),
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(template_name="registration/logout.html"), name="logout"),
+    path("register/", views.register, name="register"),
+
 ]
