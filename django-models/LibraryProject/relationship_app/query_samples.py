04d1758c52506bdaef3d80c1be91f63ae38c728d
G3NERALKISLER <jnk13576@gmail.com>
2025-09-08 23:42:24 +0300

    Update query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -7,12 +7,12 @@ for book in books_by_author:
     print(book.title)
 
 
-library = Library.objects.get(name="library_name")
+Library.objects.get(name="library_name")
 books_in_library = library.books.all()
 
 for book in books_in_library:
     print(book.title)
 
-library = Library.objects.get(name="library_name")
+Library.objects.get(name="library_name")
 librarian = library.librarian  # thanks to OneToOneField related_name
 print(librarian.name)
