dd0174bc8b9acc3241a7961a4efcc67505fd5e63
James kamau <jnk13576@gmail.com>
2025-09-27 21:28:50 +0300

    lets see

--- advanced_api_project/api/urls.py
+++ advanced_api_project/api/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from . import views
-from .views import BookListCreateAPIView, AuthorCreateAPIView
+from .views import (
+    BookListAPIView,
+    BookDetailAPIView,
+    BookCreateAPIView,
+    BookUpdateAPIView,
+    BookDeleteAPIView,
+)
 
 urlpatterns = [
-    path("", views.BookListCreateAPIView.as_view(), name="book_list_create"),
-    path("author/", views.AuthorCreateAPIView.as_view(), name="Authorcreate"),
+    path("books/", BookListAPIView.as_view(), name="book-list"),
+    path("books/<int:pk>/", BookDetailAPIView.as_view(), name="book-detail"),
+    path("books/create/", BookCreateAPIView.as_view(), name="book-create"),
+    path("books/<int:pk>/update/", BookUpdateAPIView.as_view(), name="book-update"),
+    path("books/<int:pk>/delete/", BookDeleteAPIView.as_view(), name="book-delete"),
 ]
--- advanced_api_project/api/views.py
+++ advanced_api_project/api/views.py
@@ -1,10 +1,47 @@
-from rest_framework import generics
+from rest_framework import generics,permissions
 from .models import Book, Author
 from .serializers import BookSerializer, AuthorSerializer
-
-class BookListCreateAPIView(generics.ListCreateAPIView):
+from rest_framework.permissions import BasePermission
+class BookListAPIView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-class AuthorCreateAPIView(generics.ListCreateAPIView):
+    permission_classes = [permissions.AllowAny]  # anyone can view
+
+
+# 2. Retrieve a single book by ID
+class BookDetailAPIView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+# 3. Create a new book
+class BookCreateAPIView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]  # must be logged in
+
+
+# 4. Update an existing book
+class BookUpdateAPIView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]  # must be logged in
+
+
+# 5. Delete a book
+class BookDeleteAPIView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]  # must be logged in
+
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        # SAFE_METHODS = ['GET', 'HEAD', 'OPTIONS']
+        if request.method in ("GET", "HEAD", "OPTIONS"):
+            return True
+        return request.user and request.user.is_staff
+
+class AuthorCreateAPIView(generics.CreateAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
