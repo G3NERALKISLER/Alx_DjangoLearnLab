b9fa65abac05693d69f0a4eaeca34b7f1e49255d
James kamau <jnk13576@gmail.com>
2025-09-27 21:44:32 +0300

    another bug found

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,35 +2,35 @@ from rest_framework import generics,permissions
 from .models import Book, Author
 from .serializers import BookSerializer, AuthorSerializer
 from rest_framework.permissions import BasePermission
-class BookListAPIView(generics.ListAPIView):
+class ListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.AllowAny]  # anyone can view
 
 
 # 2. Retrieve a single book by ID
-class BookDetailAPIView(generics.RetrieveAPIView):
+class DetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.AllowAny]
 
 
 # 3. Create a new book
-class BookCreateAPIView(generics.CreateAPIView):
+class CreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticated]  # must be logged in
 
 
 # 4. Update an existing book
-class BookUpdateAPIView(generics.UpdateAPIView):
+class UpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticated]  # must be logged in
 
 
 # 5. Delete a book
-class BookDeleteAPIView(generics.DestroyAPIView):
+class DeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticated]  # must be logged in
@@ -42,6 +42,6 @@ class IsAdminOrReadOnly(BasePermission):
             return True
         return request.user and request.user.is_staff
 
-class AuthorCreateAPIView(generics.CreateAPIView):
+class AuthorCreateView(generics.CreateAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
