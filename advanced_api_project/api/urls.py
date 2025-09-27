9e9bbf3ad2f3ea6e8f6ec97c25aeb2ab48526e9a
James kamau <jnk13576@gmail.com>
2025-09-27 20:52:03 +0300

    wdym

--- advanced_api_project/api/urls.py
+++ advanced_api_project/api/urls.py
@@ -3,6 +3,6 @@ from . import views
 from .views import BookListCreateAPIView, AuthorCreateAPIView
 
 urlpatterns = [
-    path("books/", views.BookListCreateAPIView.as_view(), name="book_list_create"),
+    path("", views.BookListCreateAPIView.as_view(), name="book_list_create"),
     path("author/", views.AuthorCreateAPIView.as_view(), name="Authorcreate"),
 ]
