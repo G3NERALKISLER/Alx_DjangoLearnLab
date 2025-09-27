7523a34153ef7aee435daaabb831115185d3a3cb
James kamau <jnk13576@gmail.com>
2025-09-27 21:49:07 +0300

    last bug found

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -8,9 +8,9 @@ from .views import (
 )
 
 urlpatterns = [
-    path("books/", BookListAPIView.as_view(), name="book-list"),
-    path("books/<int:pk>/", BookDetailAPIView.as_view(), name="book-detail"),
+   
+    path("books/", BookDetailAPIView.as_view(), name="book-detail"),
     path("books/create/", BookCreateAPIView.as_view(), name="book-create"),
-    path("books/<int:pk>/update/", BookUpdateAPIView.as_view(), name="book-update"),
-    path("books/<int:pk>/delete/", BookDeleteAPIView.as_view(), name="book-delete"),
+    path("books/update/", BookUpdateAPIView.as_view(), name="book-update"),
+    path("books/delete/", BookDeleteAPIView.as_view(), name="book-delete"),
 ]
