f6de741eea5c94adfaf4b68222f9f20330b6eea7
James kamau <jnk13576@gmail.com>
2025-10-01 21:39:12 +0300

    changes for task 1

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -5,7 +5,7 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.homeView, name='home'),
     path('posts/', views.PostView, name='posts'),
-    path('register', views.register_view, name='register'),
-    path('Login', views.Login_View, name='Login'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.Login_View, name='Login'),
      path("profile/", views.profile_view, name="profile"),
 ]
