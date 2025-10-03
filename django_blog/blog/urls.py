5c78aacd992428983483a7c63dd750f137e460ac
James kamau <jnk13576@gmail.com>
2025-10-03 11:21:54 +0300

    changes for task 2

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,11 +4,11 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.homeView, name='home'),
-    path('posts/', views.PostListView, name='post-list'),
-    path('posts/new/', views.PostCreateView, name='post-create'),
-    path('posts/<int:pk>/',views. PostDetailView, name='post-detail'),
-    path('posts/<int:pk>/edit/', views.PostUpdateView, name='post-update'),
-    path('posts/<int:pk>/delete/',views.PostDeleteView, name='post-delete'),
+    path('post/', views.PostListView, name='post-list'),
+    path('post/new/', views.PostCreateView, name='post-create'),
+    path('post/<int:pk>/',views.PostDetailView, name='post-detail'),
+    path('post/<int:pk>/edit/', views.PostUpdateView, name='post-update'),
+    path('post/<int:pk>/delete/',views.PostDeleteView, name='post-delete'),
     path('register/', views.register_view, name='register'),
     path('login/', views.Login_View, name='Login'),
      path("profile/", views.profile_view, name="profile"),
